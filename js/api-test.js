a8299868e7b49a7864a467116971c17c28163dde
bmansayswhat <brmcmullen@gmail.com>
2020-02-17 15:34:46 -0600

    should work

--- js/api-test.js
+++ js/api-test.js
@@ -148,7 +148,7 @@ function GetData(){
                   // console.log(destinyProfile.Response.characters.data);
                   // console.log("firebase id: " + x);
                   console.log(dprofile.data);
-                  console.log(Object.keys(dprofile.data)[0]);
+                  console.log(dprofile.data[Object.keys(dprofile.data)[0]].emblemPath);
                   document.getElementById("jimg-"+x).src = "http://www.bungie.net/"+dprofile.emblemPath;
                 })
             })
