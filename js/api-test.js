99ab8efe35081a4d19f8219262bf7e2e68d76a1a
bmansayswhat <brmcmullen@gmail.com>
2020-02-17 14:34:54 -0600

    adding backups

--- js/api-test.js
+++ js/api-test.js
@@ -106,18 +106,29 @@ function GetData(){
         </div>
       </div>
       `;
+      
       document.getElementById("players").innerHTML += playerLine;
-      // document.getElementById("players").innerHTML += '<li class="list-group-item">'+playerData[x].gamertag+' <a class="btn btn-sm btn-outline-dark float-right" data-toggle="collapse" href="#j-'+ x +'" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fas fa-user-minus"></i></a></li><div class="collapse" id="j-'+ x +'"><div class="card-body bg-danger text-white clearfix"><span class="align-middle">Remove '+playerData[x].gamertag+'?</span><a href="" class="btn btn-sm btn-outline-light float-right" onclick="PlayerDelete('+ "'" + x +"'" + ','+"'"+'joined' + "'" + ')">Confirm</a></div></div>';
     }
 
     //Initialize a variable to count backups
     var backupCount = 0;
 
     //Add to the html on the page
-    for(y in backupData)
+    for(x in backupData)
     {
       backupCount++;
-      document.getElementById("backups").innerHTML += '<li class="list-group-item">'+backupData[y].gamertag+' <a class="btn btn-sm btn-outline-dark float-right" data-toggle="collapse" href="#b-'+ y +'" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fas fa-user-minus"></i></a></li><div class="collapse" id="b-'+ y +'"><div class="card-body bg-danger text-white clearfix"><span class="align-middle">Remove '+backupData[y].gamertag+'?</span><a href="" class="btn btn-sm btn-outline-light float-right" onclick="PlayerDelete('+ "'" + y +"'" + ','+"'"+'backups' + "'" + ')">Confirm</a></div></div>';
+      const backupLine = `
+      <li class="list-group-item">
+        ${backupData[x].gamertag} <a class="btn btn-sm btn-outline-dark float-right" data-toggle="collapse" href="#j-${x}" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fas fa-user-minus"></i></a>
+      </li>
+      <div class="collapse" id="j-${x}">
+        <div class="card-body bg-danger text-white clearfix">
+          <span class="align-middle">Remove ${backupData[x].gamertag}?</span><a href="" class="btn btn-sm btn-outline-light float-right" onclick="PlayerDelete('${x}','joined')">Confirm</a>
+        </div>
+      </div>
+      `;
+
+      document.getElementById("backups").innerHTML += backupLine;
     }
 
     //Update the player and backup count on the page
