01cda5dc6a89b9e2691c035d8c6da756b95c2879
bmansayswhat <brmcmullen@gmail.com>
2020-02-17 10:11:44 -0600

    logging response json

--- js/api-test.js
+++ js/api-test.js
@@ -7,7 +7,7 @@ xhr.setRequestHeader("X-API-Key", apiKey);
 xhr.onreadystatechange = function(){
  if(this.readyState === 4 && this.status === 200){
   var json = JSON.parse(this.responseText);
-  console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+  console.log(json); //Gjallarhorn
  }
 }
 
