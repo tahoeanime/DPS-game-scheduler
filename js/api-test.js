a31dab81c1e4f5db7cadf40e20ce69c334e625de
bmansayswhat <brmcmullen@gmail.com>
2020-02-17 11:28:14 -0600

    log json?

--- js/api-test.js
+++ js/api-test.js
@@ -15,7 +15,7 @@ xhr.onreadystatechange = function(){
 
 xhr.send();
 
-console.log("id: " + xhr.responseText);
+console.log("id: " + JSON.parse(xhr.responseText).Response[0].membershipId);
 
 var g = new XMLHttpRequest();
 g.open("GET", "https://www.bungie.net/platform/Destiny2/1/Profile/" + memId + "/LinkedProfiles/", true);
