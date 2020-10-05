a399defc166ca51c429b47d4b998f01235a837f0
tahoeanime <61843785+tahoeanime@users.noreply.github.com>
2020-10-05 12:06:10 -0700

    webhook change

--- js/new-event.js
+++ js/new-event.js
@@ -184,7 +184,7 @@ function SubmitEvent() {
 
     //Discord HTTP
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/371746863659089922/N0QZnhnYHMlRX8hj8nuqmXjvFXP8GG-3tNntd-vVSlLrN3D2lo0JnmHK4e8gcVYwFBDF");
+    request.open("POST", "https://discord.com/api/webhooks/762752102177964033/nGYc6KlYvJ2b8usIX_8pIltJg6-le48cVPLRwHJqPyEgFC6flml-j9FYx2i1CO0DNLBE");
 
     request.setRequestHeader('Content-type', 'application/json');
 
