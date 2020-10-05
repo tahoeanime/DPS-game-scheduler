825d79f726f48709f8f6787215a42b7540bf0432
tahoeanime <61843785+tahoeanime@users.noreply.github.com>
2020-10-05 12:39:44 -0700

    add back discords stuff

--- js/new-event.js
+++ js/new-event.js
@@ -184,14 +184,21 @@ function SubmitEvent() {
 
     //Discord HTTP
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/371746863659089922/N0QZnhnYHMlRX8hj8nuqmXjvFXP8GG-3tNntd-vVSlLrN3D2lo0JnmHK4e8gcVYwFBDF");
+    request.open("POST", "https://discordapp.com/api/webhooks/371746863659089922/N0QZnhnYHMlRX8hj8nuqmXjvFXP8GG-3tNntd-vVSlLrN3D2lo0JnmHK4e8gcVYwFBDF");
 
     request.setRequestHeader('Content-type', 'application/json');
 
     //The Message
     var params = {
       content: '@everyone **' + title + ' **' + ' ``` ' + startTime + ' ' + timezone + ' ``` ' + ' created by: ' + ' **'+ gamertag + '**' + ' for ' + '** ' + openSpots + ' ' + 'players' + ' ** ' + ' ``` ' + details + ' ``` ',
-
+      embeds: [{
+        "title": title,
+        "color": "14177041"
+      },
+      {
+        "title": "Sign Up For The Event",
+        "url": 'https://tahoeanime.github.io/DPS-game-scheduler/event-detail.html?e='+ref.key +"&game="+ game
+        }]
     }
 
     //Post to Discord
