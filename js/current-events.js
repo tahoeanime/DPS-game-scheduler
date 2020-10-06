2be5fc2ebded1c21a51f29af6ffd386de10d3243
tahoeanime <61843785+tahoeanime@users.noreply.github.com>
2020-10-06 10:11:31 -0700

    remove color from card

--- js/current-events.js
+++ js/current-events.js
@@ -54,7 +54,7 @@ function GetData(game){
         <div class="row">
           <div class="col-lg-6">
             <div class="card mt-4">
-              <div class="card-body bg-ltgreen">
+              <div class="card-body">
                 <h5 class="card-title">${eventData.title}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${eventData.details}</h6>
                 <div class="row mt-4">
