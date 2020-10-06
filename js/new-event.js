6063d6fd292f12f8d02c69582a6dfaba2287b4e3
tahoeanime <61843785+tahoeanime@users.noreply.github.com>
2020-10-06 10:01:19 -0700

    remove redirect

--- js/new-event.js
+++ js/new-event.js
@@ -240,7 +240,7 @@ function SubmitEvent() {
     .catch((error) => {
       console.error('Error:', error);
       })
-    .then(document.location.href="https://tahoeanime.github.io/DPS-game-scheduler/index.html?e="+ref.key +"&game="+game);
+  
     //set the form action to open the event details page which will show the data for the event
     // document.getElementById("new-event").action = "https://tahoeanime.github.io/DPS-game-scheduler/event-detail.html?e="+ref.key +"&game="+game;
     // document.location.href="https://tahoeanime.github.io/DPS-game-scheduler/event-detail.html?e="+ref.key +"&game="+game;
